175e4ea57806cb80e26b6c724dd9968bda35272d
AlejandroBedoyaMartinez <98826079+AlejandroBedoyaMartinez@users.noreply.github.com>
2025-03-16 22:47:54 -0600

    Se agrego una imagen en canvas para que haga de balon, ademas se programo que esta se mueva conforme al sensor acelerometro, y se en base al tamanio de las porterias se programo que se marque gol si supera la linea de gol

--- Futbolito Pocket/app/src/main/java/jano/net/futbolitopocket/MainActivity.kt
+++ Futbolito Pocket/app/src/main/java/jano/net/futbolitopocket/MainActivity.kt
@@ -1,7 +1,7 @@
 package jano.net.futbolitopocket
 
-import android.hardware.Sensor
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,32 +10,37 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.drawscope.withTransform
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import dev.ricknout.composesensors.getSensor
+import dev.ricknout.composesensors.accelerometer.isAccelerometerSensorAvailable
+import dev.ricknout.composesensors.accelerometer.rememberAccelerometerSensorValueAsState
 import dev.ricknout.composesensors.getSensorManager
-import dev.ricknout.composesensors.isSensorAvailable
-import dev.ricknout.composesensors.rememberSensorValueAsState
 import jano.net.futbolitopocket.ui.theme.FutbolitoPocketTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,17 +57,51 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun FutbolitoPocket() {
     val sensorManager = getSensorManager()
+    val available = isAccelerometerSensorAvailable()
+    val sensorValue by rememberAccelerometerSensorValueAsState()
+    val (x, y, z) = sensorValue.value
 
-    val available = isSensorAvailable(type = Sensor.TYPE_ACCELEROMETER)
+    val imageBitmap = ImageBitmap.imageResource(id = R.drawable.balonfutbol)
 
-    val sensor = getSensor(type = Sensor.TYPE_ACCELEROMETER)
+    var golesEquipoA by remember { mutableStateOf(0) }
+    var golesEquipoB by remember { mutableStateOf(0) }
 
-    val sensorValue by rememberSensorValueAsState(type = Sensor.TYPE_ACCELEROMETER) { event ->
+    var velocidad:Float = 10f
 
+    val screenWidth = with(LocalDensity.current) { LocalConfiguration.current.screenWidthDp.dp.toPx() - 150}
+    val screenHeight = with(LocalDensity.current) { LocalConfiguration.current.screenHeightDp.dp.toPx() }
+
+    var offsetX by remember { mutableStateOf(screenWidth / 2)}
+    var offsetY by remember { mutableStateOf(screenHeight / 2 )}
+    var gol by remember { mutableStateOf(true) }
+    LaunchedEffect(sensorValue) {
+        while (gol) {
+            if (offsetX < 0) offsetX = 0f
+            if (offsetX > screenWidth) offsetX = screenWidth
+
+            if (offsetX > 420f && offsetX < 570f && offsetY < 300f) {
+                golesEquipoB++
+                gol = false
+            }else if ((offsetX < 420f || offsetX > 570f) && offsetY < 370){
+                offsetY = 370f
+            }
+
+            if (offsetX > 420f && offsetX < 570f && offsetY > 1800f) {
+                golesEquipoA++
+                gol = false
+            }else if ((offsetX < 420f || offsetX > 570f) && offsetY > 1740) {
+                offsetY = 1740f
+            }
+
+            offsetX += (-x) * velocidad
+            offsetY += y * velocidad
+
+            delay(16L)
+        }
     }
+    Log.d("balon","$offsetX y $offsetY")
 
-    var golesEquipoA:Int = 0
-    var golesEquipoB:Int = 0
+
 
     Box(
         modifier = Modifier
@@ -90,12 +129,14 @@ fun FutbolitoPocket() {
         )
 
         Canvas(modifier = Modifier.matchParentSize()) {
-            drawCircle(
-                color = Color.Red,
-                radius = 100f,
-                center = center
-            )
+            withTransform({
+                scale(scaleX = 0.5f, scaleY = 0.5f, pivot = Offset(offsetX, offsetY))
+            }) {
+                drawImage(
+                    image = imageBitmap,
+                    topLeft = Offset(offsetX, offsetY)
+                )
+            }
         }
     }
 }
-
